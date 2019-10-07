5c482653903a721555e76cabb195fdb146449f3b
Vinicius Rodrigues <r.viniciussouza@outlook.com>
2019-10-07 18:38:23 -0300

    Arquivo de criação do banco de dados

---
+++ bd.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Compra (
+	codigo INT NOT NULL PRIMARY KEY,
+	id_produto INT NOT NULL,
+	nome VARCHAR(50) NOT NULL,
+	fornecedor VARCHAR(50) NOT NULL,
+	valor DECIMAL NOT NULL,
+	data DATE NOT NULL,
+	quantidade INT NOT NULL
+);
