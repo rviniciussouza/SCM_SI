4de3ca356a45759f594e3f64b8995f8f1c6e27b4
rvinicius <rviniciussouza@outlook.com>
2019-10-10 16:38:59 -0300

    Definindo arquivos base

--- init.php
+++ init.php
@@ -4,6 +4,6 @@ date_default_timezone_set('America/Sao_Paulo');
 // conexão com o banco de dados
 define('BD_SERVIDOR','localhost');
 define('BD_USUARIO','root');
-define('BD_SENHA','');
-define('BD_BANCO','scm_si');
+define('BD_SENHA','12345');
+define('BD_BANCO','SI');
 ?>
