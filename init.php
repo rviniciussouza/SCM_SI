aeb7afaff32b77cef830f9e8b9a84631987b69ed
nayaras <nayarathaiza@gmail.com>
2019-10-22 01:25:15 -0300

    Modificando

--- init.php
+++ init.php
@@ -5,6 +5,5 @@ date_default_timezone_set('America/Sao_Paulo');
 define('DSN','pgsql:dbname=df4j5o5sohms3p; host=ec2-174-129-252-228.compute-1.amazonaws.com');
 define('BD_USER','bbkylaoouxslnh');
 define('BD_PASSWORD','9f1bbc8e5dcb80fd76e57bd51be799bc7f0ba54b4b8974ac3b1cb07d6e82933d');
-define('BD_BANCO','
-df4j5o5sohms3p');
+define('BD_BANCO','df4j5o5sohms3p');
 ?>
