4aef358105449e7cea4319d77e355c5e2f6bad1a
rvinicius <rviniciussouza@outlook.com>
2019-10-22 00:48:26 -0300

    Corrigindo path do require

--- model/Banco.php
+++ model/Banco.php
@@ -1,7 +1,7 @@
 <?php
 
 
-require_once($_SERVER['DOCUMENT_ROOT'] . "SCM_SI/init.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/SCM_SI/init.php");
 
 class Banco
 {
--- view/compra/ERPRequest.php
+++ view/compra/ERPRequest.php
@@ -1,4 +1,4 @@
-<?php require_once($_SERVER['DOCUMENT_ROOT']."SCM_SI/controller/CompraController.php");
+<?php require_once($_SERVER['DOCUMENT_ROOT']."/SCM_SI/controller/CompraController.php");
 if(isset($_GET['codproduto']) && !empty($_GET['codproduto'])) {
     $codigo = $_GET['codproduto'];
     echo $compraController->getUltimaCompra($codigo);
