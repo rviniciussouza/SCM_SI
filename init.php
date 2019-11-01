38730755b65c6ac5b55978097dbafe84ad1680d3
rvinicius <rviniciussouza@outlook.com>
2019-11-01 18:00:40 -0300

    Teste

--- init.php
+++
@@ -1,9 +0,0 @@
-<?php
-//timezone
-date_default_timezone_set('America/Sao_Paulo');
-// conexão com o banco de dados
-define('DSN','pgsql:dbname=SI; host=localhost');
-define('BD_USER','postgres');
-define('BD_PASSWORD','12345');
-define('BD_BANCO','SI');
-?>
