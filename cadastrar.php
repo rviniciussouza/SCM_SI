b69c53f1922c0d87531fc39d0e83660ae933f972
rvinicius <rviniciussouza@outlook.com>
2019-11-23 01:36:20 -0300

    Corrigindo caminho para assert/css

--- cadastrar.php
+++ cadastrar.php
@@ -20,7 +20,7 @@ if (!isset($_SESSION['login']) && !isset($_SESSION['senha'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <script src="https://kit.fontawesome.com/ad7b506960.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="/SCM_SI/asset/css/style.css">
+    <link rel="stylesheet" href="asset/css/style.css">
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 </head>
 
