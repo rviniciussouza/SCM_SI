265382d31307fd3aa0a3475f831f22f83345540e
Nayara Soares <42041514+nayaras@users.noreply.github.com>
2019-11-23 11:01:25 -0300

    Delete init.php

--- model/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-//timezone
-date_default_timezone_set('America/Sao_Paulo');
-// conexão com o banco de dados
-define('DSN','pgsql:dbname=df4j5o5sohms3p; host=ec2-174-129-252-228.compute-1.amazonaws.com');
-define('BD_USER','bbkylaoouxslnh');
-define('BD_PASSWORD','9f1bbc8e5dcb80fd76e57bd51be799bc7f0ba54b4b8974ac3b1cb07d6e82933d');
-define('BD_BANCO','df4j5o5sohms3p');
-?> 
